0e051e2d59c6d76f7883f93148bacca34a109cd0
masp9 <mart2c04@stud.kea.dk>
2022-09-13 11:20:40 +0200

    environment variables

--- src/main/java/com/example/space/reservation/controller/ReservationController.java
+++ src/main/java/com/example/space/reservation/controller/ReservationController.java
@@ -3,6 +3,7 @@ package com.example.space.reservation.controller;
 
 import com.example.space.reservation.model.Reservation;
 import com.example.space.reservation.service.ReservationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,12 +20,21 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
+    /*
     @GetMapping("/{id}")
     public ResponseEntity<Reservation> findReservationById(@PathVariable("id") Long id) {
         Optional<Reservation> reservation = Optional.of(
                                                         reservationService.findReservationById(id).orElseThrow(
                                                                 () -> new RuntimeException("not found")));
         return ResponseEntity.ok().body(reservation.get());
+    }
+
+     */
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reservation> findReservationById(@PathVariable("id") Long id) {
+        Optional<Reservation> reservation = reservationService.findReservationById(id);
+        return new ResponseEntity<>(reservation.get(), HttpStatus.OK);
     }
 
 
